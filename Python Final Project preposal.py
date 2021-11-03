ffd6268ea09665cd807da3372b4e740d6c6b07ba
AntonioMongeluzi <93612712+AntonioMongeluzi@users.noreply.github.com>
2021-11-02 21:44:07 -0400

    Add files via upload

---
+++ Python Final Project preposal.py
@@ -0,0 +1,16 @@
+# Antonio M. Mongeluzi
+# Final Project Preposal
+
+# For my final project I would like to take advantage of the double dipping from
+# another class (Mathematical Concepts in Computing) and do my final project
+# based off of a prompt given in that class.  The prompt I am interested in is
+# an apportionment problem on how to fairly divide discrete objects.
+# The class has already made it clear what they want to divide, however, I would
+#use what I learned from this class and the other to do something different for
+# this classes final project.
+
+# I am planning on working alone, and will get permission from both professors
+# before the proposal is due.
+
+# My question is in regards to what could be something interesting to do with
+# this concept from my mathematics class.  Any cool recommendations?
