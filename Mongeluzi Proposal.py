b6574a3dee2ae1840685a766f7243bd29a765f50
AntonioMongeluzi <93612712+AntonioMongeluzi@users.noreply.github.com>
2021-11-07 22:23:28 -0500

    Add files via upload

---
+++ Mongeluzi Proposal.py
@@ -0,0 +1,19 @@
+#The title of my project will be "Deciding shifts at work" or something along those lines  
+
+
+#Throughout the week and weekends I work at a bar in Center City and every week there is an issue with decidng who will work what days on the scedule.  My software will allow for employees to more easily suggest and enters the hours and days that they will work, without having two peoples schedules overlap.
+
+
+#I am planning on combining my final porject with that of Math 1166, Mathematical Concepts in computing.  I will use the apportionmrnt problem from that class in 1051's final, while also combining the coding skills I've learned in 1051 to create something a little different from the requirements of 1166.
+
+
+#A good list of features to have completed would be: 1. making sure that two employees schedules don't overlap once they're entered.  2.Allowing for fair distrubment for x number of employees across the scheudle.
+
+
+#I think that I could be able to accomplish taking all of the data entered for the shifts at work, and convert them into a spread sheet so that, in the real world, it would be easier for the employer to send out to the employees, and it would be easier for the employees to read.
+
+
+#I hope to make this like some sort of simple website rather than just a code.  I think this would be interesting and beneficial for me because website and software development is what I eventually get in to.
+
+
+#My next steps are first going to be reviewing "for loops" in a more in depth way so that I can get a solid understanding as to how they will help me create a better final project.  After the base of the project, i.e. the requirements for Math 1166, are completed, I will need to do some research on how a beginner, with no coding experience could go about programming a simple website like this, and testing what works, what doesn't work, and what is too advanced for me at this point in time.  I want to make something efficient, but need to stay somewhat in my current skill range.  
