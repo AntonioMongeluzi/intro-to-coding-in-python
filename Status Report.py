0dedd2b24e6d66d6af599f6fca07027ab19f388e
AntonioMongeluzi <93612712+AntonioMongeluzi@users.noreply.github.com>
2021-11-29 22:08:19 -0500

    Add files via upload

---
+++ Status Report.py
@@ -0,0 +1,16 @@
+#Antonio Mongeluzi
+#Progress Report
+
+# Zhenyu Zhao
+
+#Work Schedule using apportionment
+
+#I have been learning how to use different apportionment equations in python and
+#taking what I learn to decide which is the best for my final project.
+#Have also been working out how to open data that is input into a program
+#in a spread sheet.
+
+#I have not figured date configurations for my code.
+
+#No big problems yet, just deciding which apportionment method I think will work
+#best.  
